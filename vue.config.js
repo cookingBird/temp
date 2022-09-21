f73989e4842f37c2b07f57259844202c7360ad51
CaiCai <17381844023@gmail.com>
2022-09-21 13:09:06 +0800

    perfect project structure

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
-//引入nodeJs模块
+//按需引入nodeJs模块
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const isProduction = process.env.NODE_ENV === 'production';
 const CDN = {
-  js: ['cdn/cesium/Cesium.js'],
+  js: ['cdn/cesium/Cesium.js', 'cdn/vue/vue.min.js'],
   css: [],
 };
+const publicPath = isProduction ? '/main/' : './';
 module.exports = defineConfig({
+  publicPath,
   transpileDependencies: true,
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
@@ -15,10 +17,12 @@ module.exports = defineConfig({
     if (isProduction) {
       config.plugin('html').tap((args) => {
         args[0].cdn = CDN;
+        args[0].publicPath = publicPath;
         return args;
       });
       config.externals({
         cesium: /^cesium/i,
+        vue: /^vue/i,
       });
     }
   },
