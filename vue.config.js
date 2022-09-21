98c223bd84bc1629cc72d429e0409c18b1a7f4ff
CaiCai <17381844023@gmail.com>
2022-09-21 12:40:34 +0800

    feat: add external feature

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require('@vue/cli-service');
 //引入nodeJs模块
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+const CDN = {
+  js: ['cdn/cesium/Cesium.js'],
+  css: [],
+};
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
+  },
+  chainWebpack: (config) => {
+    if (isProduction) {
+      config.plugin('html').tap((args) => {
+        args[0].cdn = CDN;
+        return args;
+      });
+      config.externals({
+        cesium: /^cesium/i,
+      });
+    }
   },
 });
